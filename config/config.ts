35e058dca40beb467e97b25f166713fb6c46606c
Konda, Chandrasekhar (ck2600) <ck2600@us.att.com>
2017-10-27 06:33:56 -0700

    changes made to work with chrome

--- config/config.ts
+++ config/config.ts
@@ -6,13 +6,13 @@ export const config : Config ={
 
     seleniumAddress: 'http://localhost:4444/wd/hub',
     
-    SELENIUM_PROMISE_MANAGER:false,
+    //SELENIUM_PROMISE_MANAGER:false,
 
     capabilities: {
         browserName: 'chrome',
-        chromeOptions: {'args': ['disable-infobars']}
+        //chromeOptions: {'args': ['disable-infobars']}
     },
-
+    //directConnect: true,
     framework:"jasmine",
     specs:["../specs/**/*.js"],
     jasmineNodeOpts:{
@@ -27,8 +27,9 @@ export const config : Config ={
         browser.ignoreSynchronization = false;
         browser.manage().window().maximize();
         console.log("Waiting for the page to load..");
+        
         //browser.driver.wait()
-        browser.driver.manage().timeouts().implicitlyWait(25000);
+        //browser.driver.manage().timeouts().implicitlyWait(25000);
     },
 
     onComplete: () => {
--- pages/cpPetDetails.po.ts
+++ pages/cpPetDetails.po.ts
@@ -1,16 +1,16 @@
 // Get pet details from care planner page
-
+'use strict';
 import {browser, element, by} from 'protractor';
 
 export class petDetails {
 
- // Read details from the care planner application 
+//  // Read details from the care planner application 
 
-  element_petName = element(by.xpath(".//div[@class='petname']"));
-  element_clientName = element(by.xpath(".//div[@class='petname']/following-sibling::span"));
-  element_species = element(by.xpath(".//div[@class='species']"));
+   element_petName = element(by.xpath(".//div[@class='petname']"));
+   element_clientName = element(by.xpath(".//div[@class='petname']/following-sibling::span"));
+   element_species = element(by.xpath(".//div[@class='species']"));
 
-// Methods that can be done on the page
+// // Methods that can be done on the page
 
    getPetName(){
     let _varpetname : string ;
@@ -18,6 +18,7 @@ export class petDetails {
       return _varpetname;
       });
    }
+
    getClientName(){
      let _clientname : string ; 
      this.element_clientName.getText().then(function(_clientname){
